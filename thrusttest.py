12a1d8e1f9f50df43223b6dc7c665b9b60957a82
Kiran Yesley <kyesley60@stuy.edu>
2026-05-28 10:51:53 -0400

    somethrust

--- thrusttest.py
+++ thrusttest.py
@@ -14,6 +14,7 @@ df_f15 = pd.read_csv('F15.csv')
 motors = [df_c6, df_d12, df_f15]
 masses = [0.011,0.021,0.06]
 v_es =[]
+Thrust_dict = {}
 
 for thrust_curve in motors: 
     count =0
@@ -22,11 +23,12 @@ for thrust_curve in motors:
     x_arr = np.insert(x_arr, 0, 0.0)
     y_arr = np.insert(y_arr, 0, 0.0)
 
-
     impulse = np.trapezoid(y_arr, x = x_arr)
     v_e = impulse/masses[count]
     v_e = round(v_e, 4)
     v_es.append(v_e)
+    def Thurst(time,):
+        #ill come back to this
     count += 1
 
 dm = 
