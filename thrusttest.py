66629839b1d6bb2ddcb5c40aa85e3427adfdd5c7
Kiran Yesley <kyesley60@stuy.edu>
2026-05-31 14:34:26 -0400

    thrustcode

--- thrusttest.py
+++ thrusttest.py
@@ -12,27 +12,53 @@ df_d12 = pd.read_csv('D12.csv')
 df_f15 = pd.read_csv('F15.csv')
 
 motors = [df_c6, df_d12, df_f15]
-masses = [0.011,0.021,0.06]
-v_es =[]
-Thrust_dict = {}
+labels = ['C6', 'D12', 'F15']
+prop_masses = [0.011,0.021,0.06]
+dry_masses = [0.013, 0.023,0.042 ]
+dt =0.01
 
-for thrust_curve in motors: 
-    count =0
-    x_arr = thrust_curve['Time'].to_numpy()
-    y_arr = thrust_curve['Thrust'].to_numpy()
-    x_arr = np.insert(x_arr, 0, 0.0)
-    y_arr = np.insert(y_arr, 0, 0.0)
+def build_motor(df, mass_prop, mass_dry,name):
+    time_arr = np.insert(thrust_curve['Time'].to_numpy(), 0, 0.0)
+    Thrust_arr = np.insert(thrust_curve['Thrust'].to_numpy(), 0,0.0)
 
-    impulse = np.trapezoid(y_arr, x = x_arr)
-    v_e = impulse/masses[count]
+    impulse = np.trapezoid(Thrust_arr, x = time_arr)
+    v_e = impulse/mass_prop
     v_e = round(v_e, 4)
-    v_es.append(v_e)
-    def Thurst(time,):
-        #ill come back to this
-    count += 1
+    burn_time = time_arr[-1]
+    dm_dt_arr = Thrust_arr/v_e
+    cumalative_mass_lost = np.zeros_like(time_arr)
+    for i in range(1, len(time_arr)):
+        avg= (dm_dt_arr[i] +dm_dt_arr[i-1])/2
+        cumalative_mass_lost[i] = cumalative_mass_lost[i-1] + (avg)*(time_arr[i] -time_arr[i-1])
+    
+    def thrust(time):
+        return float(np.interp(time, time_arr, Thrust_arr))
+    def mass(time):
+        if time <= 0:
+            return mass_dry +mass_prop
+        elif time >= burn_time:
+            return mass_dry
+        else:
+            burnt = float(np.interp(time, time_arr, cumalative_mass_lost))
+            return mass_dry + mass_prop - burnt
+    
+    return {'name': name, 'v_e': v_e, 'burn_time': burn_time, 'thrust': thrust, 'mass': mass}
 
-dm = 
+motor_date = [ build_motor(df, mass_prop, mass_dry,name) for df,mass_prop, mass_dry, name in zip(motors, prop_masses, dry_masses, labels)]
+# v_es =[]
+# Thrust_dict = {}
+# count =0
+# for thrust_curve in motors: 
+ 
 
-print(v_es)
+#     impulse = np.trapezoid(y_arr, x = x_arr)
+#     v_e = impulse/masses[count]
+#     v_e = round(v_e, 4)
+#     v_es.append(v_e)
+#     def Thurst(time,):
+#         #ill come back to this
+#     count += 1
+
+# print(v_es)
 
 
