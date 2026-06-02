62c11d2a6959808e49f544b2ef59239e0e7c716a
Philip <168311517+phynder21@users.noreply.github.com>
2026-06-02 09:58:32 -0400

    Body tube drawn

--- base.py
+++ base.py
@@ -1,47 +1,5 @@
 from vpython import *
 
-# scene.background = color.white
-# scene.range = 6
-# scene.title = "\n Title \n\n"
-
-# initLength = 4
-# initHeight = 2
-
-# def makeRect(L, H):
-#     return [
-#         vector(-L/2, -H/2, 0),
-#         vector( L/2, -H/2, 0),
-#         vector( L/2,  H/2, 0),
-#         vector(-L/2,  H/2, 0),
-#         vector(-L/2, -H/2, 0),
-#     ]
-
-# rect = curve(pos=makeRect(initLength, initHeight), color=color.black)
-
-
-# def onLength(evt):
-#     lengthText.text = '{:1.2f}'.format(evt.value)
-
-# def onHeight(evt):
-#     heightText.text = '{:1.2f}'.format(evt.value)
-
-# scene.append_to_caption("\n Length: ")
-# lengthSlider = slider(bind=onLength, min=0.1, max=10, value=initLength)
-# lengthText = wtext(text='{:1.2f}'.format(initLength))
-# scene.append_to_caption(" meters\n")
-
-# scene.append_to_caption("\n Height: ")
-# heightSlider = slider(bind=onHeight, min=0.1, max=10, value=initHeight)
-# heightText = wtext(text='{:1.2f}'.format(initHeight))
-# scene.append_to_caption(" meters\n")
-
-# while True:
-#     rate(20)
-#     rect.clear()
-#     for p in makeRect(lengthSlider.value, heightSlider.value):
-#         rect.append(p)
-
-
 scene.align = "left"
 scene.width  = 450
 scene.height = 550
@@ -177,7 +135,28 @@ def draw_ui():
     reset_btn = button(text="Reset", bind=on_reset)
     scene.append_to_caption("</div>")
 
+def makeRect(L, H):
+    return [
+        vector(-L/2, -H/2, 0),
+        vector( L/2, -H/2, 0),
+        vector( L/2,  H/2, 0),
+        vector(-L/2,  H/2, 0),
+        vector(-L/2, -H/2, 0),
+    ]
+
+
+
+
+
+rect = curve(color=color.black)
+
 draw_ui()
+
+while True:
+    rate(20)
+    rect.clear()
+    for p in makeRect(state["bt_diameter"], state["bt_length"]):
+        rect.append(p)
 
 while True:
     rate(10)
